bc47637a9f419e4fa0a9f5913191c1db6c87a324
Fuente1903 <125641774+Fuente1903@users.noreply.github.com>
2023-05-21 12:17:13 +0300

    Güncelleme
    Güncelleme yapıldı.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,22 @@
-// "eylem" routerını buraya yazın
+const express = require('express');
+const router = express.Router();
+const validateActionId = require('./actions-middlware');
+
+router.get('/', validateActionId, (req, res) => {
+});
+
+router.get('/:id', validateActionId, (req,res) => {
+});
+
+router.post('/', validateActionId, (req, res) => {
+});
+
+router.put('/:id', validateActionId, (req, res) => {
+});
+
+router.delete('/:id', validateActionId, (req, res) => {
+});
+
+module.exports = router;
+
+r
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
-// "project" routerını buraya yazın!
+const express = require('express');
+const router = express.Router();
+const validateProjectId = require('./projects-middleware');
+
+router.get('/', validateProjectId, (req, res) => {
+});
+
+router.get('/:id', validateProjectId, (req,res) => {
+});
+
+router.post('/', validateProjectId, (req, res) => {
+});
+
+router.put('/:id', validateProjectId, (req, res) => {
+});
+
+router.delete('/:id', validateProjectId, (req, res) => {
+});
+
+router.get('/:id/actions', validateProjectId, (req,res) => {
+});
+
+module.exports = router; 
